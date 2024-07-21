1bfd4695af6b92ba55d84e71ace1bacc843c59c0
Ali <ali@Alis-Laptop.local>
2024-07-22 01:17:26 +0400

    Add image kid and teen screen

--- src/screens/category-lists/MenLists.Screen.tsx
+++ src/screens/category-lists/MenLists.Screen.tsx
@@ -2,7 +2,6 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {Header} from 'components/Header';
 import {ImageRatio} from 'components/ImageRatio';
 import {IMainTab, MainTab} from 'components/MainTab';
-import {Category} from 'components/specific/Category';
 import {SafeTopProvider} from 'containers/SafeTopProvider';
 import {man} from 'mock/category-lists';
 import React from 'react';
@@ -15,9 +14,6 @@ import {NavigationParamList} from 'types/navigation.types';
 export const MenListsScreen: React.FC<
   NativeStackScreenProps<NavigationParamList, StackRoutes.menlist>
 > = ({navigation}) => {
-  const manUrl =
-    'https://s3-alpha-sig.figma.com/img/e45f/4e28/37d548b6be37eb9c44fad3be45b5c88d?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MtScMB61~FBDO2sWZsAhOX2Q-zxXM4-oDfxuBFwhaP3v3HF13vklAbzoT96KsTngVZUA4o5~qvwswFA54LvyIJKLpc9CdgNL8t2h1AjRvUkOHll1LuTwGz6O9LF5arDvU2vgZiwov~d5CBHl2HcpSJDgtJ1wLc8qSiC3kPCCdp-EMqqspaI-HuIaAnoHq7d7Jok4Ohvfm9KpP64AhGvMy58dHLkNxrodIlhXhBGDpEAuWjgGjZNSJ0yNsLrKtwKdm3G2sO5EPKgc71YViO3M5fKZs6dR2PtDC6z8fJ~i0Y5gcZ2-TUnCpspWCqCw34og8wLhSqMTY-dZjs1ouFp39g__';
-
   const renderTabList = ({
     index,
     item: {title, leftIcon},
