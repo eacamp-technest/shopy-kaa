41fd79578d211f9edfc915aa20ed2ecbe9ad959a
Ali <ali@Alis-Laptop.local>
2024-07-22 16:26:48 +0400

    Create brand component

---
+++ src/components/Brand.tsx
@@ -0,0 +1,38 @@
+import {
+  Image,
+  ImageSourcePropType,
+  ImageStyle,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React from 'react';
+import {TypographyStyles} from 'theme/typography';
+import {colors} from 'theme/colors';
+
+export interface IBrand {
+  logo: ImageSourcePropType | undefined;
+  title: string;
+  style?: ImageStyle;
+}
+
+export const Brand: React.FC<IBrand> = ({logo, title, style}) => {
+  return (
+    <View style={styles.main}>
+      <Image style={style} source={logo} />
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 12,
+  },
+  text: {
+    ...TypographyStyles.TinyNoneSemibold,
+    color: colors.ink.base,
+  },
+});
